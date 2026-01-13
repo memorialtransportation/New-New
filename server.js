40704c0de3119729896e9feed1866b62d8a796bb
memorialtransportation <memorialtransportationllc1@gmail.com>
2026-01-13 18:19:20 -0500

    Update server.js

--- server.js
+++ server.js
@@ -8,17 +8,11 @@ const PORT = process.env.PORT || 10000;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// Serve static files (index.html, main.js, styles.css, images, etc.)
+// Serve your static site files (index.html, main.js, styles.css)
 app.use(express.static(__dirname));
 
-// Always send index.html for any page route
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"), (err) => {
-    if (err) {
-      console.error("sendFile error:", err);
-      res.status(500).send("Server error loading index.html");
-    }
-  });
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
 app.listen(PORT, () => {
